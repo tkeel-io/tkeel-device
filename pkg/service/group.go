6e86e3581181a08e18ef490299e5550f838ebe22
JesseJia <jessejia@yunify.com>
2021-11-17 09:48:36 +0800

    Add group and measure

---
+++ pkg/service/group.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+
+	pb "device/api/group/v1"
+)
+
+type GroupService struct {
+	pb.UnimplementedGroupServer
+}
+
+func NewGroupService() *GroupService {
+	return &GroupService{}
+}
+
+func (s *GroupService) CreateGroup(ctx context.Context, req *pb.CreateGroupRequest) (*pb.CommonResponse, error) {
+	return &pb.CommonResponse{}, nil
+}
+func (s *GroupService) UpdateGroup(ctx context.Context, req *pb.UpdateGroupRequest) (*pb.CommonResponse, error) {
+	return &pb.CommonResponse{}, nil
+}
+func (s *GroupService) DeleteGroup(ctx context.Context, req *pb.DeleteGroupRequest) (*pb.CommonResponse, error) {
+	return &pb.CommonResponse{}, nil
+}
+func (s *GroupService) GetGroup(ctx context.Context, req *pb.GetGroupRequest) (*pb.CommonResponse, error) {
+	return &pb.CommonResponse{}, nil
+}
+func (s *GroupService) ListGroup(ctx context.Context, req *pb.ListGroupRequest) (*pb.CommonResponse, error) {
+	return &pb.CommonResponse{}, nil
+}
