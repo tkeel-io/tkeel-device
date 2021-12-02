4e0116814363ae4988592bc69aec4e84deec86e4
tl <361944459@qq.com>
2021-12-02 02:50:13 -0800

    feat: update group

--- go.mod
+++ go.mod
@@ -14,6 +14,19 @@ require (
 )
 
 require (
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
+
+require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 )
--- pkg/service/group.go
+++ pkg/service/group.go
@@ -43,6 +43,7 @@ func (s *GroupService) CreateGroup(ctx context.Context, req *pb.CreateGroupReque
 	subIds := &pb.GroupEntitySubEntityIds{
 		SubEntityId: make(map[string]string),
 	}
+	subIds.SubEntityId["default"] = "SubEntity"
 	entityInfo := &pb.GroupEntityCoreInfo{
 		Group:    req.Group,
 		SysField: sysField,
@@ -66,7 +67,7 @@ func (s *GroupService) CreateGroup(ctx context.Context, req *pb.CreateGroupReque
 	err5 := json.Unmarshal(res, &entityTotalInfo)
 	if err5 != nil {
 		log.Error("error Unmarshal data from core")
-		return &pb.CreateGroupResponse{Result: "failed"}, err5 
+		return &pb.CreateGroupResponse{Result: "failed"}, err5
 	}
 	properties, ok := entityTotalInfo["properties"]
 	if !ok {
@@ -320,7 +321,7 @@ func (s *GroupService) AddGroupItems(ctx context.Context, req *pb.AddGroupItemsR
 		}
 		//modify SubEntity
 		modifyIdsMap["group"] = req.Id
-		_, err2 := s.CorePatchMethod(ctx, id, modifyIdsMap, "dev.","replace")
+		_, err2 := s.CorePatchMethod(ctx, id, modifyIdsMap, "dev.", "replace")
 		if err2 != nil {
 			log.Error("error modify SubEntity parentId")
 			return &pb.CommonResponse{Result: "failed"}, err2
@@ -333,6 +334,7 @@ func (s *GroupService) DelGroupItems(ctx context.Context, req *pb.DelGroupItemsR
 	log.Debug("DelGroupItems")
 	log.Debug("req:", req.Ids.Ids)
 	idsMap := make(map[string]interface{})
+	modifyIdsMap := make(map[string]interface{})
 	for _, id := range req.Ids.Ids {
 		//del SubEntityId
 		idsMap[id] = "SubEntity"
@@ -342,12 +344,12 @@ func (s *GroupService) DelGroupItems(ctx context.Context, req *pb.DelGroupItemsR
 			return &pb.CommonResponse{Result: "failed"}, err
 		}
 		//modify SubEntity
-		/*idsMap["group"] = req.Id
-		        _, err2 := s.CorePatchMethod(ctx, id, idsMap, "dev.","replace")
-		        if err2 != nil {
-				    log.Error("error modify SubEntity parentId")
-				    return &pb.CommonResponse{Result: "failed"}, err2
-		        }*/
+		modifyIdsMap["group"] = req.Id
+		_, err2 := s.CorePatchMethod(ctx, id, modifyIdsMap, "dev.", "remove")
+		if err2 != nil {
+			log.Error("error modify SubEntity parentId")
+			return &pb.CommonResponse{Result: "failed"}, err2
+		}
 	}
 	return &pb.CommonResponse{Result: "Ok"}, nil
 }
@@ -399,7 +401,7 @@ func (s *GroupService) CorePatchMethod(ctx context.Context, entityId string, kv 
 
 	//get token
 	tm, err := s.httpClient.GetTokenMap(ctx)
-	if nil != err{
+	if nil != err {
 		return &pb.CommonResponse{Result: "failed"}, err
 	}
 
