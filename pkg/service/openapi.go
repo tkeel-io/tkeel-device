6b6a87ace16bf212c296bdb06284cfecd852047f
tl <361944459@qq.com>
2022-01-13 23:43:28 -0800

    feat: update openapi

--- pkg/service/openapi.go
+++ pkg/service/openapi.go
@@ -33,8 +33,8 @@ func (s *OpenapiService) Identify(ctx context.Context, in *emptypb.Empty) (*open
 	return &openapi_v1.IdentifyResponse{
 		Res:          util.GetV1ResultOK(),
 		PluginId:     "tkeel-device",
-		Version:      "v0.2.0",
-		TkeelVersion: "v0.2.0",
+		Version:      "v0.3.0",
+		TkeelVersion: "v0.3.0",
 	}, nil
 }
 
